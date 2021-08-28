b705f8ae4152ae866b30361a2b218b93f19a863b
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2021-08-28 14:21:45 -0300

    Include feature to add/remove lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,10 +7,45 @@ class ListsController < ApplicationController
   end
 
   def new
-    cable_ready[ListsChannel].console_log(
-      message: "HellOO"
+    cable_ready[ListsChannel].insert_adjacent_html(
+      selector: '#lists',
+      html: render_to_string(partial: 'lists/form', locals: { list: List.new })
     )
 
     cable_ready.broadcast_to(current_user)
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      cable_ready[ListsChannel].outer_html(
+        selector: "#list-#{params.dig(:list, :client_id)}",
+        html: render_to_string(@list, assigns: { new_task: Task.new })
+      )
+    else
+      cable_ready[ListsChannel].morph(
+        selector: '#new_list',
+        html: render_to_string(partial: 'lists/form', locals: { list: @list })
+      )
+    end
+
+    cable_ready.broadcast_to(current_user)
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy!
+
+    cable_ready[ListsChannel].remove(
+      selector: dom_id(@list)
+    )
+    cable_ready.broadcast_to(current_user)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :lists, only: %i(new)
+  resources :lists, only: %i(new create destroy)
 
   root 'lists#index'
 end
