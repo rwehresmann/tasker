5cd73d9578e36498a137818a24c5480573b3e3a2
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2021-08-27 22:16:42 -0300

    Assign user when creating a task

--- app/reflexes/list_reflex.rb
+++ app/reflexes/list_reflex.rb
@@ -1,7 +1,7 @@
 class ListReflex < StimulusReflex::Reflex
   def create_task
     list = List.find(element.dataset.list_id)
-    new_task = list.tasks.create(task_params)
+    new_task = list.tasks.create(task_params.merge(creator: connection.current_user))
     @new_task = new_task.persisted? ? Task.new : new_task
   end
 
