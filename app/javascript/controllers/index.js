e8a751038e81f5bed3886e40fd631aa73f9913b7
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2021-09-19 00:03:19 -0300

    Include RadioLabel (disabled by default)

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,9 +5,13 @@ import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 import StimulusReflex from 'stimulus_reflex'
 import consumer from '../channels/consumer'
+import Radiolabel from 'radiolabel'
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
 application.consumer = consumer
 StimulusReflex.initialize(application, { isolate: true })
+
+// Uncomment below to see the visual radiolabel inspector in your page.  
+//application.register('radiolabel', Radiolabel)
